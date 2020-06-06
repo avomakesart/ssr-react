a013f533496215710947354597b558e213199e54
Alvaro Castillo Correa <alvarocastillocorrea@MacBook-Pro.local>
2020-06-06 01:06:43 -0500

    Updates

---
+++ precache-manifest.155fa681fe96895403c56f2b745b0304.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020599281a35ba3aae1f8a38b7408c8f",
+    "url": "/ardermx/index.html"
+  },
+  {
+    "revision": "57ced529b6fbd0790170",
+    "url": "/ardermx/static/js/2.6bb1e030.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ardermx/static/js/2.6bb1e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be8397e3496e97477da",
+    "url": "/ardermx/static/js/main.8739495f.chunk.js"
+  },
+  {
+    "revision": "4df74ac7678598aa3626",
+    "url": "/ardermx/static/js/runtime-main.91ad2a71.js"
+  },
+  {
+    "revision": "dcbf302764d2e3ebebf7dfa0f284ab65",
+    "url": "/ardermx/static/media/arder-inconsciente.dcbf3027.mp3"
+  },
+  {
+    "revision": "28fbdbb6ccf415017136e7aadb3fb1f5",
+    "url": "/ardermx/static/media/arder.28fbdbb6.gif"
+  },
+  {
+    "revision": "cb1e67f43ab399aa68593f734f429c4d",
+    "url": "/ardermx/static/media/chris.cb1e67f4.jpg"
+  },
+  {
+    "revision": "5e0382a8bd222c0b7ecec3152923a13c",
+    "url": "/ardermx/static/media/jorge.5e0382a8.jpg"
+  },
+  {
+    "revision": "61ebcd51b8498688edaa9d8204844fdd",
+    "url": "/ardermx/static/media/leo.61ebcd51.jpg"
+  },
+  {
+    "revision": "fb92f6bfe50ee1b3f690043ebadfdc3a",
+    "url": "/ardermx/static/media/manu.fb92f6bf.jpg"
+  },
+  {
+    "revision": "5de43c5784544d3331edf163b825c88d",
+    "url": "/ardermx/static/media/pepes.5de43c57.jpg"
+  }
+]);
